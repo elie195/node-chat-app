0bc0f14c59391261df79034ca89bc541278a0523
El-ad Blech <elie195@gmail.com>
2017-01-06 00:33:59 +0200

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
     console.log('Connected to server');
-
-    socket.emit('createMessage', {
-        from: 'Jen',
-        text: 'Hey there.'
-    });
 });
 
 socket.on('disconnect', function () {
